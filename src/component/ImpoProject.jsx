25709fa750c78b49b36df79ce82dca74c7ba4b3e
Harsh Patle <harshpatle491@gmail.com>
2024-12-12 00:18:43 +0530

    projects start

--- src/component/ImpoProject.jsx
+++ src/component/ImpoProject.jsx
@@ -1,21 +1,29 @@
-import project from './project.json'
+import project from './project.json';
 
-export const ImpoProjects = () =>{
-
-    return(
-        <>
-        {
-      project.map((curElem,key)  =>(
- 
-        <div>
-            <div>
-                <img src={curElem.image} alt="" />
-            </div>
-            <h1>{curElem.heading}</h1>
-            <h1></h1>
-        </div>
-    ))
-    }
-        </>
-    )
-}
+export const ImpoProjects = () => {
+  return (
+    <>
+      <ul>
+        <li>
+        {project.map((curElem, key) => (
+          <div key={key} className="w-full h-1/2 bg-red-600 mt-9 mr-4">
+            <img src={curElem.image} alt={curElem.heading} />
+            <h1 className="text-center">{curElem.heading}</h1>
+            <p className="text-justify p-3">{curElem.description}</p>
+            <ul>
+              {curElem.skills.map((skill, index) => (
+                <li key={index}>{skill}</li>
+              ))}
+            </ul>
+            <p>
+              <a href={curElem.visitLink} target="_blank" rel="noopener noreferrer">
+                Visit Project
+              </a>
+            </p>
+          </div>
+        ))}
+        </li>
+      </ul>
+    </>
+  );
+};
--- src/router/pages/Projects.jsx
+++ src/router/pages/Projects.jsx
@@ -4,10 +4,10 @@ import { ImpoProjects } from '../../component/ImpoProject'
 const Projects = () => {
   return (
     <>
-    
-    
-
+    <h1 className='text-center font-extrabold text-4xl'>Projects</h1>
     <ImpoProjects />
+    <hr className="text-yellow-400 drop-shadow-[0_0_20px_yellow]" />
+
     </>
   )
 }
