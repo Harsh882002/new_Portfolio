7775c19e81ae70b4bc5dfed93a1acf7742a8e962
Harsh Patle <harshpatle491@gmail.com>
2024-12-17 17:42:26 +0530

    final thing

--- src/component/ImpoProject.jsx
+++ src/component/ImpoProject.jsx
@@ -5,21 +5,22 @@ export const ImpoProjects = () => {
     <>
     <div className='w-4/5'>
       <ul className="w-screen h-1/2     ">
-        <li className='mr-5 ml-5 sm:grid grid-cols-2 gap-9 sm:mr-14 ml-14 mt-10 mb-14'>
+        <li className='mr-5 ml-5  sm:grid grid-cols-2 gap-9 sm:mr-14 ml-14 mt-10 mb-14'>
         {project.map((curElem, key) => (
           <div key={key}  >
             <img src={curElem.image} alt={curElem.heading} className='mt-5' />
             <div className='bg-black'>
-            <h1 className="text-center font-parkinsans mt-4 p-3" >{curElem.heading}</h1>
+            <h1 className="text-center font-parkinsans mt-4 p-3 text-2xl text-orange-500 font-parkinsans" >{curElem.heading}</h1>
             <p className="text-justify p-2 m-2 ">{curElem.description}</p>
-            <ul className='grid grid-cols-3 mr-8 ml-8 gap-2 mb-4'>
+            <ul className='grid grid-cols-2 mr-8 ml-8 gap-3 mb-4'>
               {curElem.skills.map((skill, index) => (
-                <li key={index} className='bg-orange-500 text-center '>{skill}</li>
+                <li key={index} className='border-4 font-bold border-orange-500 text-center rounded w-30 '>{skill}</li>
               ))}
             </ul>
             <p>
-              <a href={curElem.visitLink} target="_blank" rel="noopener noreferrer" className='ml-3 mp-5'>
-                Visit Project
+              <a href={curElem.visitLink} target="_blank" rel="noopener noreferrer" className='ml-3 text-lg '>
+                CLICK ME TO VISIT PROJECT
+
               </a>
             </p>
           </div>
