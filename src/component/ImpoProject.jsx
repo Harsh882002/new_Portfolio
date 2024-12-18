66692b5bb61963d9f279144150e2970875566761
Harsh Patle <harshpatle491@gmail.com>
2024-12-18 12:54:18 +0530

    final thing

--- src/component/ImpoProject.jsx
+++ src/component/ImpoProject.jsx
@@ -10,18 +10,18 @@ export const ImpoProjects = () => {
           <div key={key}  >
             <img src={curElem.image} alt={curElem.heading} className='mt-5' />
             <div className='bg-black'>
-            <h1 className="text-center font-parkinsans mt-4 p-3 text-2xl text-orange-500 font-parkinsans" >{curElem.heading}</h1>
+            <h1 className="text-center font-parkinsans mt-0 p-3 text-2xl text-orange-500 font-parkinsans" >{curElem.heading}</h1>
             <p className="text-justify p-2 m-2 ">{curElem.description}</p>
             <ul className='grid grid-cols-2 mr-8 ml-8 gap-3 mb-4'>
               {curElem.skills.map((skill, index) => (
-                <li key={index} className='border-4 font-bold border-orange-500 text-center rounded w-30 '>{skill}</li>
+                <li key={index} className='border-4 font-bold border-orange-500 text-center rounded w-30 mb-5 '>{skill}</li>
               ))}
             </ul>
             <p>
-              <a href={curElem.visitLink} target="_blank" rel="noopener noreferrer" className='ml-3 text-lg '>
+              {/* <a href={curElem.visitLink} target="_blank" rel="noopener noreferrer" className='ml-3 text-lg '>
                 CLICK ME TO VISIT PROJECT
 
-              </a>
+              </a> */}
             </p>
           </div>
           </div>
