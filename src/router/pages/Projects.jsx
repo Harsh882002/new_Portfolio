1bf00d3216c3a1732e0a0b485412a758252c5924
Harsh Patle <harshpatle491@gmail.com>
2025-01-17 23:53:36 +0530

    working

--- src/router/pages/Projects.jsx
+++ src/router/pages/Projects.jsx
@@ -5,8 +5,8 @@ const Projects = () => {
   return (
     <>
     <h1 className='sm:mt-5 text-3xl justify-center text-center font-permanentMarker text-xxl'>Projects</h1>
-    <ImpoProjects />
-    <hr className="text-yellow-400 drop-shadow-[0_0_20px_yellow]"  />
+    <ImpoProjects  />
+    <hr className="text-yellow-400 drop-shadow-[0_0_20px_yellow] "  />
 
     </> 
   )
